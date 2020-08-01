4ed496121e9ae1023022a70b1449fde2d70d6a34
Naomi Isbell <naomiisbell@gmail.com>
2020-08-01 03:01:16 -0400

    componentDidMount working

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -2,53 +2,69 @@ import React from 'react'
 import axios from 'axios'
 import { Input, Button } from "@material-ui/core"
 
+const meme_URL = "https://api.imgflip.com/get_memes"
+
 class MemeGenerator extends React.Component {
-    constructor(){
-        super()
+    constructor(props) {
+        super(props)
+
         this.state = {
             topText: '',
             bottomText: '',
             randomImg: "https://i.imgflip.com/345v97.jpg",
             apiImgs: [],
+            number: 4,
             formSubmitted: false,
         }
+        this.handleSubmission = this.handleSubmission.bind(this)
     }
 
     componentDidMount = () => {
         // used GET method to get the api url 
-        axios.get(`https://api.imgflip.com/get_memes`)
+        axios.get(`${meme_URL}`)
+        // .then(res => res.json())
         // used .then method to make my response
-        .then(res => {
+        .then(res => 
             // response is equal to my memes variable
             // called the apiImgs property to gather the data from my empty arr
-            const memes = res.apiImgs.data
+            // const memes = res.data
             // setState to the apiImgs prop = memes data...
-            this.setState({apiImgs: memes})
-        })
+            this.setState({ apiImgs: res.data })
+        )
+        // always add .catch error message !!
         .catch(error => {
             console.log("something is wrong", error)
         })
     }
-
     
+
+    handleSubmission = (event) => {
+        event.preventDefault()
+        const randomMeme = Math.floor(Math.random() * this.state.apiImgs.length)
+        const randApiImg = this.state.apiImgs[randomMeme].url
+        this.setState({ randomImg: randApiImg })
+    }
 
     render() {
         return (
-            <div>
-                <Input 
-                name="topText"
-                type="text"
-                placeholder="add top text"
-                value={this.state.topText}
-                />
-                <Input 
-                name="bottomText"
-                type="text"
-                placeholder="add bottom text"
-                value={this.state.bottomText}
-                />
+            <div className="meme">
+                <form className="meme__form">
+                    <Input 
+                    name="topText"
+                    type="text"
+                    placeholder="add top text"
+                    value={this.state.topText}
+                    />
+                    <Input 
+                    name="bottomText"
+                    type="text"
+                    placeholder="add bottom text"
+                    value={this.state.bottomText}
+                    />
 
-                <Button>Generate</Button>
+                    <Button onClick={this.handleSubmission}>Generate</Button>
+                </form>
+                    <img src={this.state.randomImg} alt=""/>
             </div>
         )
     }
