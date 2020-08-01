935bf914b7217143355bc9a5ed8e0f3a472c782e
Naomi Isbell <naomiisbell@gmail.com>
2020-07-31 22:29:52 -0400

    added API to componentDidMount function

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class MemeGenerator extends React.Component {
     constructor() {
@@ -7,9 +8,17 @@ class MemeGenerator extends React.Component {
         this.state = {
             topText: '',
             bottomText: '',
-            randomImage: "https://i.imgflip.com//30b1gx.jpg",
+            randomImage: "https://i.imgflip.com/30b1gx.jpg",
             memeImgs: []
         }
+    }
+
+    componentDidMount() {
+        axios.get(' https://api.imgflip.com/get_memes')
+        .then(response => {
+            const { memes } = response.data
+            this.setState({ memeImgs: memes})
+        })
     }
 
     render() {
