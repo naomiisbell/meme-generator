69eb75997fb28c40ed7726b725f06bc8c8eccb0b
Naomi Isbell <naomiisbell@gmail.com>
2020-07-31 22:24:08 -0400

    created my state and props

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
 class MemeGenerator extends React.Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            topText: '',
+            bottomText: '',
+            randomImage: "https://i.imgflip.com//30b1gx.jpg",
+            memeImgs: []
+        }
+    }
+
     render() {
         return (
             <div>
