57c0fe53e61c59d515c6238f43fcb931d57ca5e5
Naomi Isbell <naomiisbell@gmail.com>
2020-08-02 14:10:47 -0400

    added home page image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,11 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                
+                <img 
+                className="home__cover" 
+                width= "100%" 
+                src="https://hdwallpaperim.com/wp-content/uploads/2017/08/24/113315-memes-748x421.jpg"
+                />
             </div>
         )
     }
