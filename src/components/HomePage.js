fbd9b608d64c4013fd67f5c4af0a2d01a37511f4
Naomi Isbell <naomiisbell@gmail.com>
2020-08-02 17:13:15 -0400

    added alt attribute

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends React.Component {
                 className="home__cover" 
                 width= "100%" 
                 src="https://hdwallpaperim.com/wp-content/uploads/2017/08/24/113315-memes-748x421.jpg"
+                alt=""
                 />
             </div>
         )
