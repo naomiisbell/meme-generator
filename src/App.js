946ac98c3b5ff63861cf259dd4ecc986315ff596
Naomi Isbell <naomiisbell@gmail.com>
2020-08-02 12:39:39 -0400

    added in react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import MemeGenerator from "./components/MemeGenerator";
+import HomePage from "./components/HomePage"
 
-
-function App() {
+export default function App() {
   return (
-      <div className="app">
-        <MemeGenerator />
+    <Router>
+      <div className="nav__bar">
+        <h1 className="header">Meme University</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/create">Create a Meme</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/create">
+            <MemeGenerator />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+        </Switch>
       </div>
-  )
+    </Router>
+  );
 }
 
-export default App;
