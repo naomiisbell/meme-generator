0c5782282aa982674395127fda0fc85b05cddbbf
Naomi Isbell <naomiisbell@gmail.com>
2020-08-02 21:05:21 -0400

    working config

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase"
 
-const firebaseApp = firebase.initializeApp({
+
+const firebaseApp = {
     apiKey: "AIzaSyAR5l-Iz4KrlPrkg5ZwJb80LsX99uI8n4g",
     authDomain: "meme-generator-eb5c0.firebaseapp.com",
     databaseURL: "https://meme-generator-eb5c0.firebaseio.com",
@@ -9,10 +10,12 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "125537289625",
     appId: "1:125537289625:web:7059053c2aecb2df1fbf08",
     measurementId: "G-V6NJ2JT7NE"
-    });
+    };
 
     const db = firebaseApp.firestore();
     const auth = firebase.auth();
     const storage = firebase.storage();
 
-    export default { auth, db, storage }
+    firebase.initializeApp(firebaseApp)
+
+    export default { auth, db, storage, firebase }
